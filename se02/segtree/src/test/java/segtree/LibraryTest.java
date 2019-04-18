66e4e16972371f98640376690cd999fd7ff5af4b
Jeanderson Candido <jeandersonbc@gmail.com>
2019-04-18 03:41:50 +0200

    Segment Tree - Kickoff

---
+++ se02/segtree/src/test/java/segtree/LibraryTest.java
@@ -0,0 +1,11 @@
+package segtree;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibraryTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        fail("Not implemented");
+    }
+}
